4b6d882361f23114063ce5444da1d48a7c6c13c0
Ibragim <gizuyev@gmail.com>
2023-10-19 21:59:29 +0600

    html, made model 'Orders'

--- store/settings.py
+++ store/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'users',
     'products',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -188,6 +189,9 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-CELERY_BROKER = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
-
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
